42c757354369c2420a13addf4b85c6f4cf7a6af8
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-09-03 17:18:17 +0430

    Initial commit

---
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jmzd.ghazal.storeappmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
