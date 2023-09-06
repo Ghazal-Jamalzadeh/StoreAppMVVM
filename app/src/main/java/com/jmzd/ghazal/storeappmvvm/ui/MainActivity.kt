1fce8ec3a86026ac30f1375feee136a0b8df2744
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-09-06 17:28:08 +0430

    MainActivity base structure

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/MainActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/MainActivity.kt
@@ -3,10 +3,27 @@ package com.jmzd.ghazal.storeappmvvm.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jmzd.ghazal.storeappmvvm.R
+import com.jmzd.ghazal.storeappmvvm.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    //binding
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
