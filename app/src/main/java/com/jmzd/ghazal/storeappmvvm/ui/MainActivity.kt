6cb05dec5798ea296eec92b89bc960783a3cf629
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-09-06 13:21:35 +0430

    create packaging structure

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/MainActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jmzd.ghazal.storeappmvvm
+package com.jmzd.ghazal.storeappmvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jmzd.ghazal.storeappmvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
