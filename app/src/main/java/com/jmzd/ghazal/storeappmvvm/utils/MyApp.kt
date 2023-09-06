a05b5e8530e8eee8c6542b7ac54863ae9cf04408
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-09-06 13:38:49 +0430

    added fonts and completed application class

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/utils/MyApp.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/utils/MyApp.kt
@@ -1,4 +1,24 @@
 package com.jmzd.ghazal.storeappmvvm.utils
 
-class MyApp {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import io.github.inflationx.calligraphy3.CalligraphyConfig
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor
+import io.github.inflationx.viewpump.ViewPump
+
+@HiltAndroidApp
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        //Calligraphy
+        ViewPump.init(
+            ViewPump.builder().addInterceptor(
+                CalligraphyInterceptor(
+                    CalligraphyConfig.Builder()
+                        .setDefaultFontPath("fonts/iransans.ttf")
+                        .build()
+                )
+            ).build()
+        )
+    }
 }
