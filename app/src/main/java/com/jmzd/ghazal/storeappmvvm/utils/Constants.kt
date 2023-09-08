de61a941da5c1913dba087eb4519f7617fc64033
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-09-08 16:00:15 +0430

    define server constants

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/utils/Constants.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/utils/Constants.kt
@@ -1,4 +1,9 @@
 package com.jmzd.ghazal.storeappmvvm.utils
 
+//Base URL
+const val BASE_URL = "https://shop.nouri-api.ir/api/v1/"
+const val CONNECTION_TIME_OUT = 60
+
+//Session
 const val SESSION_AUTH_DATA = "session_auth_data"
 const val USER_TOKEN_DATA = "user_token_data"
