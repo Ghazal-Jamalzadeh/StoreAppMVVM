5ab8918726cc61e9656668b4fa5d0f028eaa8563
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-09-11 19:37:51 +0430

    rename response classes

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/utils/network/ResponseHandler.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/utils/network/ResponseHandler.kt
@@ -4,10 +4,10 @@ import com.google.gson.Gson
 import com.jmzd.ghazal.storeappmvvm.data.models.ErrorResponse
 import retrofit2.Response
 
-open class NetworkResponse<T> (private val response: Response<T>) {
-    open fun generateResponse() : NetworkRequest<T>{
+open class ResponseHandler<T> (private val response: Response<T>) {
+    open fun generateResponse() : MyResponse<T>{
         return when {
-            response.code() == 401 -> NetworkRequest.Error("You are not authorized")
+            response.code() == 401 -> MyResponse.Error("You are not authorized")
             response.code() == 422 -> {
                 var errorMessage = ""
                 if (response.errorBody() != null) {
@@ -18,12 +18,12 @@ open class NetworkResponse<T> (private val response: Response<T>) {
                         errorMessage = value.joinToString()
                     }
                 }
-                NetworkRequest.Error(errorMessage)
+                MyResponse.Error(errorMessage)
             }
 
-            response.code() == 500 -> NetworkRequest.Error("Try again")
-            response.isSuccessful -> NetworkRequest.Success(response.body()!!)
-            else -> NetworkRequest.Error(response.message())
+            response.code() == 500 -> MyResponse.Error("Try again")
+            response.isSuccessful -> MyResponse.Success(response.body()!!)
+            else -> MyResponse.Error(response.message())
         }
     }
 }
