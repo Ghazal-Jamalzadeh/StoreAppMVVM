e797b8cbd361c4b5b616b92c79a15562d01cf179
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-09-23 16:36:52 +0330

    added getProfile to ApiServices.kt

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/network/ApiServices.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/network/ApiServices.kt
@@ -4,11 +4,9 @@ import androidx.datastore.preferences.protobuf.BoolValue
 import com.jmzd.ghazal.storeappmvvm.data.models.login.BodyLogin
 import com.jmzd.ghazal.storeappmvvm.data.models.login.ResponseLogin
 import com.jmzd.ghazal.storeappmvvm.data.models.login.ResponseVerify
+import com.jmzd.ghazal.storeappmvvm.data.models.profile.ResponseProfile
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.Header
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface ApiServices {
     @POST("auth/login")
@@ -16,4 +14,7 @@ interface ApiServices {
 
     @POST("auth/login/verify")
     suspend fun postVerify(@Body body: BodyLogin) : Response<ResponseVerify>
+
+    @GET("auth/detail")
+    suspend fun getProfile() : Response<ResponseProfile>
 }
