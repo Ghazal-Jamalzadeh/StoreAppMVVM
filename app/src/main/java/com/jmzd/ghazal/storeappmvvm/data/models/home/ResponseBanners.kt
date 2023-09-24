2b7f0eb95e40930581b2dd67587876d4b803e34d
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-09-24 13:01:38 +0330

    create ResponseBanners model

---
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/models/home/ResponseBanners.kt
@@ -0,0 +1,31 @@
+package com.jmzd.ghazal.storeappmvvm.data.models.home
+
+
+import com.google.gson.annotations.SerializedName
+import com.jmzd.ghazal.storeappmvvm.data.models.home.ResponseBanners.ResponseBannersItem
+
+class ResponseBanners : ArrayList<ResponseBannersItem>(){
+    data class ResponseBannersItem(
+        @SerializedName("image")
+        val image: String?, // media/images/banners/tLuGegnINYbXLOoEmcFO5OG8Xej52W8beg1cUUbC.jpg
+        @SerializedName("link")
+        val link: String?, // category
+        @SerializedName("link_id")
+        val linkId: String?, // 11
+        @SerializedName("title")
+        val title: String?, // کالاهای دیجیتالی
+        @SerializedName("url_link")
+        val urlLink: UrlLink?
+    ) {
+        data class UrlLink(
+            @SerializedName("id")
+            val id: Int?, // 56
+            @SerializedName("parent_id")
+            val parentId: String?, // 1
+            @SerializedName("slug")
+            val slug: String?, // category-mobile
+            @SerializedName("title")
+            val title: String? // دفتر برنامه ریزی مستر راد طرح جدید کاکتوس مدل پالت کد 1587
+        )
+    }
+}
