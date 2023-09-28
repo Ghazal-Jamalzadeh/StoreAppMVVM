4f2572d658b69e79f23de272516de794e27014ee
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-09-28 18:11:49 +0330

    HomeFragment : Discounts : viewModel

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/viewmodel/HomeViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jmzd.ghazal.storeappmvvm.data.models.home.ResponseBanners
+import com.jmzd.ghazal.storeappmvvm.data.models.home.ResponseDiscount
 import com.jmzd.ghazal.storeappmvvm.data.repository.HomeRepository
 import com.jmzd.ghazal.storeappmvvm.utils.network.MyResponse
 import com.jmzd.ghazal.storeappmvvm.utils.network.ResponseHandler
@@ -20,12 +21,16 @@ class HomeViewModel @Inject constructor(private val repository: HomeRepository) 
         viewModelScope.launch {
             delay(300)
             getBanners()
+            getDiscounts()
         }
     }
 
-    //profile
+    //banners
     private val _bannersLiveData = MutableLiveData<MyResponse<ResponseBanners>>()
     val bannersLiveData: LiveData<MyResponse<ResponseBanners>> = _bannersLiveData
+    //discounts
+    private val _discountsLiveData = MutableLiveData<MyResponse<ResponseDiscount>>()
+    val discountsLiveData: LiveData<MyResponse<ResponseDiscount>> = _discountsLiveData
 
     private fun getBanners() = viewModelScope.launch {
 
@@ -34,5 +39,15 @@ class HomeViewModel @Inject constructor(private val repository: HomeRepository) 
         val response: Response<ResponseBanners> = repository.getBanners()
 
         _bannersLiveData.value = ResponseHandler(response).generateResponse()
+    }
+
+
+    private fun getDiscounts() = viewModelScope.launch {
+
+        _discountsLiveData.value = MyResponse.Loading()
+
+        val response: Response<ResponseDiscount> = repository.getDiscounts()
+
+        _discountsLiveData.value = ResponseHandler(response).generateResponse()
     }
 }
