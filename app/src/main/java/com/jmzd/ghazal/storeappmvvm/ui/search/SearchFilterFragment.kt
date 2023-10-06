82fca1b3a1aa026346a2f8ea3cd9a147f530808e
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-10-06 13:21:38 +0330

    SearchFilter: fragment

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/search/SearchFilterFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/search/SearchFilterFragment.kt
@@ -3,10 +3,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.jmzd.ghazal.storeappmvvm.R
+import com.jmzd.ghazal.storeappmvvm.data.models.search_filter.FilterModel
 import com.jmzd.ghazal.storeappmvvm.databinding.FragmentSearchFilterBinding
+import com.jmzd.ghazal.storeappmvvm.ui.search.adapters.FilterAdapter
+import com.jmzd.ghazal.storeappmvvm.utils.extensions.setupRecyclerview
+import com.jmzd.ghazal.storeappmvvm.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SearchFilterFragment : BottomSheetDialogFragment() {
@@ -16,10 +23,13 @@ class SearchFilterFragment : BottomSheetDialogFragment() {
     private val binding get() = _binding!!
 
     //viewModel
-//    private val viewModel by viewModels<LoginViewModel>()
+    private val viewModel by activityViewModels<SearchViewModel>()
 
     //Theme
     override fun getTheme() = R.style.RemoveDialogBackground
+
+    //Adapters
+    @Inject lateinit var adapter: FilterAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,9 +41,30 @@ class SearchFilterFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //init views
+        //Init views
         binding.apply {
+            //Close
+            closeImg.setOnClickListener { this@SearchFilterFragment.dismiss() }
+        }
+        //Fill filter data
+        viewModel.getFilters()
+        //observers
+        observeFiltersLiveData()
+    }
 
+    private fun observeFiltersLiveData() {
+        viewModel.filterLiveData.observe(this) {
+            initFilterRecycler(it)
+        }
+    }
+
+    private fun initFilterRecycler(data: List<FilterModel>) {
+        adapter.setData(data)
+        binding.filtersList.setupRecyclerview(LinearLayoutManager(requireContext()), adapter)
+        //Click
+        adapter.setOnItemClickListener {
+//            viewModel.sendSelectedFilterItem(it)
+            this.dismiss()
         }
     }
 
