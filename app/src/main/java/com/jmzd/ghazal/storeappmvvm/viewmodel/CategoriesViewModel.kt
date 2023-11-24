8a3e18fa87715caeb834e62a0e02abce8f6c11d1
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-11-24 17:51:13 +0330

    yala

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/viewmodel/CategoriesViewModel.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/viewmodel/CategoriesViewModel.kt
@@ -8,11 +8,13 @@ import com.jmzd.ghazal.storeappmvvm.data.models.categories.ResponseCategories
 import com.jmzd.ghazal.storeappmvvm.data.repository.CategoriesRepository
 import com.jmzd.ghazal.storeappmvvm.utils.network.MyResponse
 import com.jmzd.ghazal.storeappmvvm.utils.network.ResponseHandler
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import retrofit2.Response
 import javax.inject.Inject
 
+@HiltViewModel
 class CategoriesViewModel @Inject constructor(private val repository: CategoriesRepository) : ViewModel() {
 
     init {
