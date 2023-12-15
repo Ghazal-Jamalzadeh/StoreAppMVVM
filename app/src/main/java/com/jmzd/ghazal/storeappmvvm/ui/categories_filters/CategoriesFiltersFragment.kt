20dcd45daa306b16e03a1912da8d04a5bd5fa982
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-12-15 15:26:44 +0330

    #357 categoriesFilters : init range slider

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/categories_filters/CategoriesFiltersFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/categories_filters/CategoriesFiltersFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.google.android.material.slider.LabelFormatter
+import com.google.android.material.slider.RangeSlider
 import com.jmzd.ghazal.storeappmvvm.databinding.FragmentCategoriesFiltersBinding
 import com.jmzd.ghazal.storeappmvvm.utils.base.BaseFragment
+import com.jmzd.ghazal.storeappmvvm.utils.extensions.moneySeparating
 import com.jmzd.ghazal.storeappmvvm.viewmodel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,6 +22,10 @@ class CategoriesFiltersFragment : BaseFragment() {
 
     //viewModel
     private val viewModel by viewModels<LoginViewModel>()
+
+    //variables
+    private var minPrice: String? = null
+    private var maxPrice: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,9 +40,39 @@ class CategoriesFiltersFragment : BaseFragment() {
         //init views
         binding.apply {
 
+            //range slider
+            initPriceRange()
+
+
         }
     }
 
+    //--- range slider ---//
+    private fun initPriceRange() {
+        //Label format
+        val formatter = LabelFormatter { value ->
+            value.toInt().moneySeparating()
+        }
+        //Customize
+        binding.priceRange.apply {
+            setValues(7000000f, 21000000f)
+            setLabelFormatter(formatter)
+            //Listener
+            addOnSliderTouchListener(object : RangeSlider.OnSliderTouchListener {
+                override fun onStartTrackingTouch(slider: RangeSlider) {
+
+                }
+
+                override fun onStopTrackingTouch(slider: RangeSlider) {
+                    val values = slider.values
+                    minPrice = values[0].toInt().toString()
+                    maxPrice = values[1].toInt().toString()
+                }
+            })
+        }
+    }
+
+    //--- life cycle ---//
     override fun onNetworkLost() {
     }
 
