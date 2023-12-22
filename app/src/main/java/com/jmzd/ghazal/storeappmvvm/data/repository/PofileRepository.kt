7ef129883797778e32699ed1acbd938fc7060ff3
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-12-22 16:24:29 +0330

    #361 uploadPhoto: repository

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/PofileRepository.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/PofileRepository.kt
@@ -1,8 +1,10 @@
 package com.jmzd.ghazal.storeappmvvm.data.repository
 
 import com.jmzd.ghazal.storeappmvvm.data.api.ApiServices
+import okhttp3.RequestBody
 import javax.inject.Inject
 
 class PofileRepository  @Inject constructor(private val api : ApiServices) {
     suspend fun getProfile() = api.getProfile()
+    suspend fun uploadAvatar(body : RequestBody) = api.uploadAvatar(body)
 }
