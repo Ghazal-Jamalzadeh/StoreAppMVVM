fbdfbe72adaeefaa7060c98909c36fb8b76a9c61
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-12-22 15:00:19 +0330

    #360 Profile/ WalletBalance : model

---
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/models/profile/ResponseWallet.kt
@@ -0,0 +1,9 @@
+package com.jmzd.ghazal.storeappmvvm.data.models.profile
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseWallet(
+    @SerializedName("wallet")
+    val wallet: String? // 0
+)
