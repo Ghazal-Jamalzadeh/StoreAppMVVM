234df05fe32d292a63b73981725dd5d8a4383ae6
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-12-29 15:03:13 +0330

    #363 editProfile : created fragment

---
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/profile_edit/ProfileEditFragment.kt
@@ -0,0 +1,45 @@
+package com.jmzd.ghazal.storeappmvvm.ui.profile_edit
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.jmzd.ghazal.storeappmvvm.databinding.FragmentProfileEditBinding
+import com.jmzd.ghazal.storeappmvvm.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CategoriesProductFragment : BottomSheetDialogFragment() {
+
+    //binding
+    private var _binding: FragmentProfileEditBinding? = null
+    private val binding get() = _binding!!
+
+    //viewModel
+    private val viewModel by viewModels<LoginViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentProfileEditBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //init views
+        binding.apply {
+
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
