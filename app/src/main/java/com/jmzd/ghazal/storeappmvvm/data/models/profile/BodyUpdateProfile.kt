e4a179ed7e91e3444efe0086806968ecdfe23d87
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-12-29 16:37:05 +0330

    #363 editProfile: model di

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/models/profile/BodyUpdateProfile.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/models/profile/BodyUpdateProfile.kt
@@ -4,16 +4,16 @@ package com.jmzd.ghazal.storeappmvvm.data.models.profile
 import com.google.gson.annotations.SerializedName
 
 data class BodyUpdateProfile(
-    @SerializedName("email")
-    val email: String?, // test@nouri-api.ir
+//    @SerializedName("email")
+//    var email: String? = null, // test@nouri-api.ir
     @SerializedName("firstName")
-    val firstName: String?, // api
+    var firstName: String? = null, // api
     @SerializedName("gregorianDate")
-    val gregorianDate: String?, // 1993-10-25
+    var gregorianDate: String? = null, // 1993-10-25
     @SerializedName("idNumber")
-    val idNumber: String?, // 0000000000
-    @SerializedName("job")
-    val job: String?, // Web Developer
+    var idNumber: String? = null, // 0000000000
+//    @SerializedName("job")
+//    var job: String? = null, // Web Developer
     @SerializedName("lastName")
-    val lastName: String? // nouri
+    var lastName: String? = null // nouri
 )
--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/utils/di/FragmentModule.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/utils/di/FragmentModule.kt
@@ -1,6 +1,7 @@
 package com.jmzd.ghazal.storeappmvvm.utils.di
 
 import com.jmzd.ghazal.storeappmvvm.data.models.login.BodyLogin
+import com.jmzd.ghazal.storeappmvvm.data.models.profile.BodyUpdateProfile
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,4 +13,7 @@ object FragmentModule {
 
     @Provides
     fun bodyLogin() = BodyLogin()
+
+    @Provides
+    fun bodyUpdateProfile() = BodyUpdateProfile()
 }
