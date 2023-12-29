d435ee277d4a05d9cf636b43cd278d14ef5f3bf4
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-12-29 16:06:24 +0330

    #363 editProfile: load profile data

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/profile_edit/ProfileEditFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/profile_edit/ProfileEditFragment.kt
@@ -1,14 +1,18 @@
 package com.jmzd.ghazal.storeappmvvm.ui.profile_edit
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.jmzd.ghazal.storeappmvvm.R
 import com.jmzd.ghazal.storeappmvvm.databinding.FragmentProfileEditBinding
-import com.jmzd.ghazal.storeappmvvm.viewmodel.LoginViewModel
+import com.jmzd.ghazal.storeappmvvm.utils.extensions.showSnackBar
+import com.jmzd.ghazal.storeappmvvm.utils.network.MyResponse
+import com.jmzd.ghazal.storeappmvvm.viewmodel.ProfileViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,7 +23,7 @@ class ProfileEditFragment : BottomSheetDialogFragment() {
     private val binding get() = _binding!!
 
     //viewModel
-    private val viewModel by viewModels<LoginViewModel>()
+    private val viewModel by viewModels<ProfileViewModel>()
 
     //Theme
     override fun getTheme() = R.style.RemoveDialogBackground
@@ -34,13 +38,51 @@ class ProfileEditFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //call api
+        viewModel.getProfileData()
+
         //Init views
         binding.apply {
             //Close
-//            closeImg.setOnClickListener { this@ProfileEditFragment.dismiss() }
+            closeImg.setOnClickListener { this@ProfileEditFragment.dismiss() }
         }
+
+        //observers
+        observeProfileLiveData()
     }
 
+
+    @SuppressLint("SetTextI18n")
+    private fun observeProfileLiveData() {
+        binding.apply {
+            viewModel.profileLiveData.observe(viewLifecycleOwner) { response ->
+                when (response) {
+                    is MyResponse.Loading -> {
+                        loading.isVisible = true
+                    }
+
+                    is MyResponse.Success -> {
+                        loading.isVisible = false
+                        response.data?.let { data ->
+                            if (data.firstname.isNullOrEmpty().not())
+                                nameEdt.setText(data.firstname)
+                            if (data.lastname.isNullOrEmpty().not())
+                                familyEdt.setText(data.lastname)
+                            if (data.idNumber.isNullOrEmpty().not())
+                                idNumberEdt.setText(data.idNumber)
+                            if (data.birthDate.isNullOrEmpty().not())
+                                birthDateEdt.setText(data.birthDate!!.split("T")[0])
+                        }
+                    }
+
+                    is MyResponse.Error -> {
+                        loading.isVisible = false
+                        root.showSnackBar(response.message!!)
+                    }
+                }
+            }
+        }
+    }
 
     override fun onDestroy() {
         super.onDestroy()
