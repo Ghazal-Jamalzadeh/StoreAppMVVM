bb6a049278bc636b19f5b12a9089ca4154a575ee
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-12-29 16:13:37 +0330

    #363 editProfile: cerate response model

---
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/models/profile/ResponseUpdateProfile.kt
@@ -0,0 +1,19 @@
+package com.jmzd.ghazal.storeappmvvm.data.models.profile
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseUpdateProfile(
+    @SerializedName("email")
+    val email: String?, // test@nouri-api.ir
+    @SerializedName("firstName")
+    val firstName: String?, // api
+    @SerializedName("gregorianDate")
+    val gregorianDate: String?, // 1993-10-25
+    @SerializedName("idNumber")
+    val idNumber: String?, // 0000000000
+    @SerializedName("job")
+    val job: String?, // Web Developer
+    @SerializedName("lastName")
+    val lastName: String? // nouri
+)
