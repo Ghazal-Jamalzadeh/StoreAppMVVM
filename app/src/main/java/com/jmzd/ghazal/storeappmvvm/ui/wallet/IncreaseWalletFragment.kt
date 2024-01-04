a45972e9a799e5167f563a285e751047fd0aa892
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-01-04 22:52:27 +0330

    #367 Wallet: convert to BottomSheetDialogFragment

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/wallet/IncreaseWalletFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/wallet/IncreaseWalletFragment.kt
@@ -4,13 +4,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.jmzd.ghazal.storeappmvvm.R
 import com.jmzd.ghazal.storeappmvvm.databinding.FragmentIncreaseWalletBinding
-import com.jmzd.ghazal.storeappmvvm.utils.base.BaseFragment
 import com.jmzd.ghazal.storeappmvvm.viewmodel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class IncreaseWalletFragment : BaseFragment() {
+class IncreaseWalletFragment : BottomSheetDialogFragment() {
 
     //binding
     private var _binding: FragmentIncreaseWalletBinding? = null
@@ -18,6 +19,9 @@ class IncreaseWalletFragment : BaseFragment() {
 
     //viewModel
     private val viewModel by viewModels<LoginViewModel>()
+
+    //Theme
+    override fun getTheme() = R.style.RemoveDialogBackground
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,10 +38,6 @@ class IncreaseWalletFragment : BaseFragment() {
 
         }
     }
-
-    override fun onNetworkLost() {
-    }
-
 
     override fun onDestroy() {
         super.onDestroy()
