0a08936448aabef52f92ae9adac4e06c00f2f746
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-01-05 19:54:48 +0330

    #370 favorites: create fragment

---
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/profile_favorite/ProfileFavoriteFragment.kt
@@ -0,0 +1,47 @@
+package com.jmzd.ghazal.storeappmvvm.ui.profile_favorite
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.jmzd.ghazal.storeappmvvm.databinding.FragmentProfileFavoriteBinding
+import com.jmzd.ghazal.storeappmvvm.utils.base.BaseFragment
+import com.jmzd.ghazal.storeappmvvm.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProfileFavoriteFragment : BaseFragment() {
+
+    //binding
+    private var _binding: FragmentProfileFavoriteBinding? = null
+    private val binding get() = _binding!!
+
+    //viewModel
+    private val viewModel by viewModels<LoginViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentProfileFavoriteBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //init views
+        binding.apply {
+
+        }
+    }
+
+    override fun onNetworkLost() {
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
