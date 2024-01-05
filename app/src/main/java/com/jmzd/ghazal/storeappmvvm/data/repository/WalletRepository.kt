d760c2edffcd67ccf081457fe817757b284d146d
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-01-05 14:39:53 +0330

    #367 Wallet: repository

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/WalletRepository.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/WalletRepository.kt
@@ -1,8 +1,10 @@
 package com.jmzd.ghazal.storeappmvvm.data.repository
 
 import com.jmzd.ghazal.storeappmvvm.data.api.ApiServices
+import com.jmzd.ghazal.storeappmvvm.data.models.wallet.BodyIncreaseWallet
 import javax.inject.Inject
 
 class WalletRepository @Inject constructor(private val api : ApiServices) {
     suspend fun getWalletBalance() = api.getWalletBalance()
+    suspend fun increaseWallet(body : BodyIncreaseWallet) = api.increaseWallet(body)
 }
