d879094f368f6c3e5c1566f7fef51211ac3d2740
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-01-05 14:43:44 +0330

    #367 Wallet: viewModel

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/viewmodel/WalletViewModel.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/viewmodel/WalletViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jmzd.ghazal.storeappmvvm.data.models.profile.ResponseWallet
+import com.jmzd.ghazal.storeappmvvm.data.models.wallet.BodyIncreaseWallet
+import com.jmzd.ghazal.storeappmvvm.data.models.wallet.ResponseIncreaseWallet
 import com.jmzd.ghazal.storeappmvvm.data.repository.WalletRepository
 import com.jmzd.ghazal.storeappmvvm.utils.network.MyResponse
 import com.jmzd.ghazal.storeappmvvm.utils.network.ResponseHandler
@@ -16,9 +18,13 @@ import javax.inject.Inject
 @HiltViewModel
 class WalletViewModel @Inject constructor(private val repository: WalletRepository) : ViewModel() {
 
-    //banners
+    //wallet balance
     private val _walletBalanceLiveData = MutableLiveData<MyResponse<ResponseWallet>>()
     val walletBalanceLiveData: LiveData<MyResponse<ResponseWallet>> = _walletBalanceLiveData
+
+    //increase wallet
+    private val _increaseWalletLiveData = MutableLiveData<MyResponse<ResponseIncreaseWallet>>()
+    val increaseWalletLiveData: LiveData<MyResponse<ResponseIncreaseWallet>> = _increaseWalletLiveData
 
     //--- api call ---//
     fun getWalletBalance() = viewModelScope.launch {
@@ -30,6 +36,15 @@ class WalletViewModel @Inject constructor(private val repository: WalletReposito
         _walletBalanceLiveData.value = ResponseHandler(response).generateResponse()
     }
 
+    //--- increase wallet ---//
+    fun increaseWallet(body : BodyIncreaseWallet) = viewModelScope.launch {
+
+        _increaseWalletLiveData.value = MyResponse.Loading()
+
+        val response: Response<ResponseIncreaseWallet> = repository.increaseWallet(body)
+
+        _increaseWalletLiveData.value = ResponseHandler(response).generateResponse()
+    }
 
 
 }
