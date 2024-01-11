5d11be1263db56b8f54cc6ebb32f94a6285b1f53
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-01-11 15:39:58 +0330

    #371 addresses: viewModel

---
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/viewmodel/ProfileAddressesViewModel.kt
@@ -0,0 +1,36 @@
+package com.jmzd.ghazal.storeappmvvm.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jmzd.ghazal.storeappmvvm.data.models.address.ResponseProfileAddresses
+import com.jmzd.ghazal.storeappmvvm.data.models.profile_favorite.ResponseProfileFavorites
+import com.jmzd.ghazal.storeappmvvm.data.repository.AdressesRepository
+import com.jmzd.ghazal.storeappmvvm.utils.network.MyResponse
+import com.jmzd.ghazal.storeappmvvm.utils.network.ResponseHandler
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileAddressesViewModel @Inject constructor(private val repository: AdressesRepository) : ViewModel() {
+
+    //profile addresses
+    private val _profileAddressesLiveData = MutableLiveData<MyResponse<ResponseProfileAddresses>>()
+    val profileAddressesLiveData: LiveData<MyResponse<ResponseProfileAddresses>> = _profileAddressesLiveData
+
+    //--- api call ---//
+    fun getProfileAddresses() = viewModelScope.launch {
+
+        _profileAddressesLiveData.value = MyResponse.Loading()
+
+        val response: Response<ResponseProfileAddresses> = repository.getProfileAddresses()
+
+        _profileAddressesLiveData.value = ResponseHandler(response).generateResponse()
+    }
+
+
+
+}
