1a3a8bf8805024079d233bb8edfb66b29d448f72
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-01-12 18:43:49 +0330

    #373 submit address: repository

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/AddressesRepository.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/AddressesRepository.kt
@@ -1,10 +1,12 @@
 package com.jmzd.ghazal.storeappmvvm.data.repository
 
 import com.jmzd.ghazal.storeappmvvm.data.api.ApiServices
+import com.jmzd.ghazal.storeappmvvm.data.models.address.BodySubmitAddress
 import javax.inject.Inject
 
 class AddressesRepository @Inject constructor(private val api : ApiServices) {
     suspend fun getProfileAddresses() = api.getProfileAddresses()
     suspend fun getProvinceList() = api.getProvinceList()
     suspend fun getCityList(provinceId : Int) = api.getCityList(provinceId)
+    suspend fun submitAddress(body : BodySubmitAddress) = api.submitAddress(body)
 }
