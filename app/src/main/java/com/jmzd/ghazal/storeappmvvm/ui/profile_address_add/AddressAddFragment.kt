5851b0507f41942bd3e1d4101d91c774b2bb19da
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-01-12 18:08:33 +0330

    #373 provinceList: show province list inside drop down

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/profile_address_add/AddressAddFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/profile_address_add/AddressAddFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
+import com.jmzd.ghazal.storeappmvvm.R
 import com.jmzd.ghazal.storeappmvvm.databinding.FragmentAddressAddBinding
 import com.jmzd.ghazal.storeappmvvm.utils.base.BaseFragment
-import com.jmzd.ghazal.storeappmvvm.viewmodel.LoginViewModel
+import com.jmzd.ghazal.storeappmvvm.utils.extensions.showSnackBar
+import com.jmzd.ghazal.storeappmvvm.utils.network.MyResponse
+import com.jmzd.ghazal.storeappmvvm.viewmodel.ProfileAddressesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,7 +22,12 @@ class AddressAddFragment : BaseFragment() {
     private val binding get() = _binding!!
 
     //viewModel
-    private val viewModel by viewModels<LoginViewModel>()
+    private val viewModel by viewModels<ProfileAddressesViewModel>()
+
+    //province
+    private val provincesNamesList = mutableListOf<String>()
+    private lateinit var provincesAdapter: ArrayAdapter<String>
+    private var provinceId = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,11 +39,55 @@ class AddressAddFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //call api
+        if (isNetworkAvailable) {
+            viewModel.getProvinceList()
+        }
         //init views
         binding.apply {
 
         }
+
+        //observers
+        observeProvinceListLiveData()
     }
+
+    private fun observeProvinceListLiveData() {
+        binding.apply {
+            viewModel.provinceListLiveData.observe(viewLifecycleOwner) { response ->
+                when (response) {
+                    is MyResponse.Loading -> {}
+
+                    is MyResponse.Success -> {
+                        response.data?.let { data ->
+                            if (data.isNotEmpty()) {
+                                data.forEach {
+                                    provincesNamesList.add(it.title!!)
+                                }
+                                provincesAdapter = ArrayAdapter<String>(
+                                    requireContext(), R.layout.dropdown_menu_popup_item, provincesNamesList
+                                )
+                                provinceAutoTxt.apply {
+                                    setAdapter(provincesAdapter)
+                                    setOnItemClickListener { _, _, position, _ ->
+                                        provinceId = data[position].id!!
+//                                        body.provinceId = provinceId.toString()
+//                                        if (isNetworkAvailable)
+//                                            viewModel.callCitiesListApi(provinceId)
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    is MyResponse.Error -> {
+                        root.showSnackBar(response.message!!)
+                    }
+                }
+            }
+        }
+    }
+
 
     override fun onNetworkLost() {
     }
