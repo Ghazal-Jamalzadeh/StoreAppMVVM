13ad0078f467660bcbdb94e2d591da78bc7a8d18
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-01-25 16:16:00 +0330

    #380 detail: repository

---
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/DetailRepository.kt
@@ -0,0 +1,8 @@
+package com.jmzd.ghazal.storeappmvvm.data.repository
+
+import com.jmzd.ghazal.storeappmvvm.data.api.ApiServices
+import javax.inject.Inject
+
+class DetailRepository @Inject constructor(private val api : ApiServices) {
+    suspend fun getDetail(productId : String) = api.getDetail(productId)
+}
