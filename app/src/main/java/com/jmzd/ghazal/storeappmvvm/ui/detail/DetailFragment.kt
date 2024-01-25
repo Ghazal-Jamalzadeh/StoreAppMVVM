8bc1c186e98d9004d7fa0bbcfb09deab199807a4
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-01-25 17:44:13 +0330

    #381 detail: observer

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/detail/DetailFragment.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.jmzd.ghazal.storeappmvvm.data.models.detail.ResponseDetail
 import com.jmzd.ghazal.storeappmvvm.databinding.FragmentDetailBinding
 import com.jmzd.ghazal.storeappmvvm.utils.base.BaseFragment
+import com.jmzd.ghazal.storeappmvvm.utils.extensions.changeVisibility
+import com.jmzd.ghazal.storeappmvvm.utils.extensions.showSnackBar
+import com.jmzd.ghazal.storeappmvvm.utils.network.MyResponse
 import com.jmzd.ghazal.storeappmvvm.viewmodel.DetailViewModel
 import com.jmzd.ghazal.storeappmvvm.viewmodel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -48,12 +53,48 @@ class DetailFragment : BaseFragment() {
                 viewModel.getDetail(productId)
             }
         }
-        //init views
-        binding.apply {
+        //Back
+        binding.detailHeaderLay.backImg.setOnClickListener { findNavController().popBackStack() }
 
+        //observers
+        observeDetailLiveData()
+    }
+    //--- observers ---//
+    private fun observeDetailLiveData() {
+        binding.apply {
+            viewModel.detailLiveData.observe(viewLifecycleOwner) { response ->
+                when (response) {
+                    is MyResponse.Loading -> {
+                        detailLoading.changeVisibility(true, containerLay)
+                    }
+
+                    is MyResponse.Success -> {
+                        detailLoading.changeVisibility(false, containerLay)
+                        response.data?.let { data ->
+                            initDetailViews(data)
+                        }
+                    }
+
+                    is MyResponse.Error -> {
+                        detailLoading.changeVisibility(false, containerLay)
+                        root.showSnackBar(response.message!!)
+                    }
+                }
+            }
         }
     }
 
+    //--- init views ---//
+    private fun initDetailViews(data: ResponseDetail) {
+//        PRODUCT_ID = data.id!!
+//        initDetailHeaderView(data)
+//        initDetailInfoView(data)
+//        initDetailTimerView(data)
+//        setupViewPager()
+//        initDetailBottomView(data)
+    }
+
+    //--- life cycle ---//
     override fun onNetworkLost() {
     }
 
