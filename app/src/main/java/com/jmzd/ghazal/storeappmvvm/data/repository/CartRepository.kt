aeabae2620b1c130940fc3a13e0545312a6d792d
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-02 13:26:21 +0330

    #386 cart: addToCart: repository

---
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/CartRepository.kt
@@ -0,0 +1,9 @@
+package com.jmzd.ghazal.storeappmvvm.data.repository
+
+import com.jmzd.ghazal.storeappmvvm.data.api.ApiServices
+import com.jmzd.ghazal.storeappmvvm.data.models.cart.BodyAddToCart
+import javax.inject.Inject
+
+class CartRepository @Inject constructor(private val api : ApiServices) {
+    suspend fun addToCart(productId : Int, body : BodyAddToCart ) = api.addToCart(productId , body)
+}
