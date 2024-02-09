cb55acb800fa8be3f8a8b56352b3c4da59b57dd3
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-09 11:42:03 +0330

    #389 AddComment: repository

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/DetailRepository.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/DetailRepository.kt
@@ -1,6 +1,7 @@
 package com.jmzd.ghazal.storeappmvvm.data.repository
 
 import com.jmzd.ghazal.storeappmvvm.data.api.ApiServices
+import com.jmzd.ghazal.storeappmvvm.data.models.comments.BodySendComment
 import javax.inject.Inject
 
 class DetailRepository @Inject constructor(private val api : ApiServices) {
@@ -8,4 +9,5 @@ class DetailRepository @Inject constructor(private val api : ApiServices) {
     suspend fun postLikeProduct(productId : Int) = api.postLikeProduct(productId)
     suspend fun getDetailFeatures(productId : Int) = api.getDetailFeatures(productId)
     suspend fun getDetailComments(productId : Int) = api.getDetailComments(productId)
+    suspend fun sendComment(productId : Int , body : BodySendComment) = api.sendComment(productId , body)
 }
