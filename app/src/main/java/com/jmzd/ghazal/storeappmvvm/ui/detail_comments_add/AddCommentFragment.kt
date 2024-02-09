8506db3efb6a87b87d4e558aa8dbbdb11602c6f5
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-09 11:06:56 +0330

    #389 AddComment: fragment created

---
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/detail_comments_add/AddCommentFragment.kt
@@ -0,0 +1,53 @@
+package com.jmzd.ghazal.storeappmvvm.ui.detail_comments_add
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.jmzd.ghazal.storeappmvvm.R
+import com.jmzd.ghazal.storeappmvvm.databinding.FragmentAddCommentBinding
+import com.jmzd.ghazal.storeappmvvm.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CategoriesProductFragment : BottomSheetDialogFragment() {
+
+    //binding
+    private var _binding: FragmentAddCommentBinding? = null
+    private val binding get() = _binding!!
+
+    //viewModel
+    private val viewModel by viewModels<LoginViewModel>()
+
+    //Theme
+    override fun getTheme() = R.style.RemoveDialogBackground
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentAddCommentBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //init views
+        binding.apply {
+
+            //Close
+//            closeImg.setOnClickListener { this@ProfileEditFragment.dismiss() }
+
+        }
+    }
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
