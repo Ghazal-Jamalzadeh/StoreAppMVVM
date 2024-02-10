c37ff998c7cb8eebf86bdf25656339d90d3db72d
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-10 18:17:56 +0330

    #395 UpdateCart: viewModel

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/viewmodel/CartViewModel.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/viewmodel/CartViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import com.jmzd.ghazal.storeappmvvm.data.models.SimpleResponse
 import com.jmzd.ghazal.storeappmvvm.data.models.cart.BodyAddToCart
 import com.jmzd.ghazal.storeappmvvm.data.models.cart.ResponseCartList
+import com.jmzd.ghazal.storeappmvvm.data.models.cart.ResponseUpdateCart
 import com.jmzd.ghazal.storeappmvvm.data.models.detail.ResponseDetail
 import com.jmzd.ghazal.storeappmvvm.data.repository.CartRepository
 import com.jmzd.ghazal.storeappmvvm.utils.network.MyResponse
@@ -26,6 +27,16 @@ class CartViewModel @Inject constructor(private val repository: CartRepository) 
     //cart list
     private val _cartListLiveData = MutableLiveData<MyResponse<ResponseCartList>>()
     val cartListLiveData: LiveData<MyResponse<ResponseCartList>> = _cartListLiveData
+    //increment
+    private val _incrementLiveData = MutableLiveData<MyResponse<ResponseUpdateCart>>()
+    val incrementLiveData: LiveData<MyResponse<ResponseUpdateCart>> = _incrementLiveData
+    //decrement
+    private val _decrementLiveData = MutableLiveData<MyResponse<ResponseUpdateCart>>()
+    val decrementLiveData: LiveData<MyResponse<ResponseUpdateCart>> = _decrementLiveData
+    //delete product
+    private val _deleteProductLiveData = MutableLiveData<MyResponse<ResponseUpdateCart>>()
+    val deleteProductLiveData: LiveData<MyResponse<ResponseUpdateCart>> = _deleteProductLiveData
+
 
     //--- api call ---//
     fun addToCart(productId : Int , body : BodyAddToCart) = viewModelScope.launch {
@@ -44,6 +55,35 @@ class CartViewModel @Inject constructor(private val repository: CartRepository) 
         val response: Response<ResponseCartList> = repository.getCartList()
 
         _cartListLiveData.value = ResponseHandler(response).generateResponse()
+    }
+
+    fun increment(id : Int) = viewModelScope.launch {
+
+        _incrementLiveData.value = MyResponse.Loading()
+
+        val response: Response<ResponseUpdateCart> = repository.cartIncrement(id)
+
+        _incrementLiveData.value = ResponseHandler(response).generateResponse()
+    }
+
+
+    fun decrement(id : Int) = viewModelScope.launch {
+
+        _decrementLiveData.value = MyResponse.Loading()
+
+        val response: Response<ResponseUpdateCart> = repository.cartDecrement(id)
+
+        _decrementLiveData.value = ResponseHandler(response).generateResponse()
+    }
+
+
+    fun deleteProduct(id : Int) = viewModelScope.launch {
+
+        _deleteProductLiveData.value = MyResponse.Loading()
+
+        val response: Response<ResponseUpdateCart> = repository.cartDeleteProduct(id)
+
+        _deleteProductLiveData.value = ResponseHandler(response).generateResponse()
     }
 
 
