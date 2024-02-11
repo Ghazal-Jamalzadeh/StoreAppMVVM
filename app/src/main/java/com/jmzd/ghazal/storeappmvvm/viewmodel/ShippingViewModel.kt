0c45ddf44e737f7ef0eaf746998b1a7da2bed3cd
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-11 17:32:14 +0330

    #400 Shipping-SetAddress : viewModel

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/viewmodel/ShippingViewModel.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/viewmodel/ShippingViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jmzd.ghazal.storeappmvvm.data.models.address.BodySetAddressForShipping
+import com.jmzd.ghazal.storeappmvvm.data.models.address.ResponseSetAddressForShipping
 import com.jmzd.ghazal.storeappmvvm.data.models.shipping.ResponseShipping
 import com.jmzd.ghazal.storeappmvvm.data.repository.ShippingRepository
 import com.jmzd.ghazal.storeappmvvm.utils.network.NetworkRequest
@@ -20,6 +22,9 @@ class ShippingViewModel @Inject constructor(private val repository: ShippingRepo
     //shipping
     private val _shippingLiveData = MutableLiveData<NetworkRequest<ResponseShipping>>()
     val shippingLiveData: LiveData<NetworkRequest<ResponseShipping>> = _shippingLiveData
+    //set address
+    private val _setAddressLiveData = MutableLiveData<NetworkRequest<ResponseSetAddressForShipping>>()
+    val setAddressLiveData: LiveData<NetworkRequest<ResponseSetAddressForShipping>> = _setAddressLiveData
 
     //--- api call ---//
     fun getShipping() = viewModelScope.launch {
@@ -29,6 +34,15 @@ class ShippingViewModel @Inject constructor(private val repository: ShippingRepo
         val response: Response<ResponseShipping> = repository.getShipping()
 
         _shippingLiveData.value = ResponseHandler(response).generateResponse()
+    }
+
+    fun setAddress(body : BodySetAddressForShipping) = viewModelScope.launch {
+
+        _setAddressLiveData.value = NetworkRequest.Loading()
+
+        val response: Response<ResponseSetAddressForShipping> = repository.setAddress(body)
+
+        _setAddressLiveData.value = ResponseHandler(response).generateResponse()
     }
 
 }
