b147ced9e601c673f5becc9eec26ecf23099d8e2
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-11 17:29:23 +0330

    #400 Shipping-SetAddress : repository

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/ShippingRepository.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/ShippingRepository.kt
@@ -1,8 +1,10 @@
 package com.jmzd.ghazal.storeappmvvm.data.repository
 
 import com.jmzd.ghazal.storeappmvvm.data.api.ApiServices
+import com.jmzd.ghazal.storeappmvvm.data.models.address.BodySetAddressForShipping
 import javax.inject.Inject
 
 class ShippingRepository @Inject constructor(private val api : ApiServices) {
     suspend fun getShipping() = api.getShipping()
+    suspend fun setAddress(body : BodySetAddressForShipping) = api.shippingSetAddress(body)
 }
