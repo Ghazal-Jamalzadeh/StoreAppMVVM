839cfc661ea3023ff7848d45596be618945294ca
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-11 14:31:24 +0330

    #397 Shipping: fragment created

---
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/ui/shipping/ShippingFragment.kt
@@ -0,0 +1,48 @@
+package com.jmzd.ghazal.storeappmvvm.ui.shipping
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.jmzd.ghazal.storeappmvvm.databinding.FragmentSearchBinding
+import com.jmzd.ghazal.storeappmvvm.utils.base.BaseFragment
+import com.jmzd.ghazal.storeappmvvm.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ShippingFragment : BaseFragment() {
+
+    //binding
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    //viewModel
+    private val viewModel by viewModels<LoginViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentSearchBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //init views
+        binding.apply {
+
+        }
+    }
+
+    override fun onNetworkLost() {
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
