519af1ba2f9cdc3ea9b6073833d44448cb0c9e9c
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-11 15:29:57 +0330

    #398 Shipping: viewModel

---
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/viewmodel/ShippingViewModel.kt
@@ -0,0 +1,40 @@
+package com.jmzd.ghazal.storeappmvvm.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jmzd.ghazal.storeappmvvm.data.models.SimpleResponse
+import com.jmzd.ghazal.storeappmvvm.data.models.cart.BodyAddToCart
+import com.jmzd.ghazal.storeappmvvm.data.models.cart.ResponseCartList
+import com.jmzd.ghazal.storeappmvvm.data.models.cart.ResponseUpdateCart
+import com.jmzd.ghazal.storeappmvvm.data.models.detail.ResponseDetail
+import com.jmzd.ghazal.storeappmvvm.data.models.shipping.ResponseShipping
+import com.jmzd.ghazal.storeappmvvm.data.repository.CartRepository
+import com.jmzd.ghazal.storeappmvvm.data.repository.ShippingRepository
+import com.jmzd.ghazal.storeappmvvm.utils.network.MyResponse
+import com.jmzd.ghazal.storeappmvvm.utils.network.ResponseHandler
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import javax.inject.Inject
+
+
+@HiltViewModel
+class ShippingViewModel @Inject constructor(private val repository: ShippingRepository) : ViewModel() {
+
+    //shipping
+    private val _shippingLiveData = MutableLiveData<MyResponse<ResponseShipping>>()
+    val shippingLiveData: LiveData<MyResponse<ResponseShipping>> = _shippingLiveData
+
+    //--- api call ---//
+    fun getShipping() = viewModelScope.launch {
+
+        _shippingLiveData.value = MyResponse.Loading()
+
+        val response: Response<ResponseShipping> = repository.getShipping()
+
+        _shippingLiveData.value = ResponseHandler(response).generateResponse()
+    }
+
+}
