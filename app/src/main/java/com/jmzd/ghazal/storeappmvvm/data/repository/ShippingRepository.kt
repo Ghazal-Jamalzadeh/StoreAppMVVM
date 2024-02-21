f212c7cf1ac47702a6b80407e12991a4ba5e9397
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-21 17:26:01 +0330

    #402 Payment: repository

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/ShippingRepository.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/ShippingRepository.kt
@@ -9,4 +9,5 @@ class ShippingRepository @Inject constructor(private val api : ApiServices) {
     suspend fun getShipping() = api.getShipping()
     suspend fun setAddress(body : BodySetAddressForShipping) = api.shippingSetAddress(body)
     suspend fun checkCoupon(body : BodyCoupon) = api.checkCoupon(body)
+    suspend fun payment(body : BodyCoupon) = api.payment(body)
 }
