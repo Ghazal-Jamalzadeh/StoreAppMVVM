fabd3dd2c855a1a302e3655728fea358d691d896
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-21 16:12:15 +0330

    #401 Coupon: model body

---
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/models/shipping/BodyCoupon.kt
@@ -0,0 +1,9 @@
+package com.jmzd.ghazal.storeappmvvm.data.models.shipping
+
+
+import com.google.gson.annotations.SerializedName
+
+data class BodyCoupon(
+    @SerializedName("couponId")
+    val couponId: String? // poff-20
+)
