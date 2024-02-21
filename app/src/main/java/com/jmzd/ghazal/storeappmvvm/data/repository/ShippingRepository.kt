8170cf9843e998da37255d6be085be5ceb693c30
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-21 16:19:26 +0330

    #401 Coupon: repository

--- app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/ShippingRepository.kt
+++ app/src/main/java/com/jmzd/ghazal/storeappmvvm/data/repository/ShippingRepository.kt
@@ -2,9 +2,11 @@ package com.jmzd.ghazal.storeappmvvm.data.repository
 
 import com.jmzd.ghazal.storeappmvvm.data.api.ApiServices
 import com.jmzd.ghazal.storeappmvvm.data.models.address.BodySetAddressForShipping
+import com.jmzd.ghazal.storeappmvvm.data.models.shipping.BodyCoupon
 import javax.inject.Inject
 
 class ShippingRepository @Inject constructor(private val api : ApiServices) {
     suspend fun getShipping() = api.getShipping()
     suspend fun setAddress(body : BodySetAddressForShipping) = api.shippingSetAddress(body)
+    suspend fun checkCoupon(body : BodyCoupon) = api.checkCoupon(body)
 }
